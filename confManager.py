5de774678c1315de46dd1aa0ced86f67fa9fd118
rs-appez <alexandrep1999@hotmail.com>
2023-08-18 10:34:44 +0200

    fix if conf.json is invalid

--- confManager.py
+++ confManager.py
@@ -2,25 +2,38 @@ from pathlib import Path
 import json
 from tools import get_character_name
 
+CONF_FILE = "config.json"
+
 class ConfManager():
 
     @staticmethod
-    def get_initiative(windows, ewmh) -> dict():
-        conf_file = Path("config.json")
+    def __check_conf_file():
+        conf_file = Path(CONF_FILE)
         if not conf_file.is_file() :
-            with open("config.json",'w') as cf :
-                cf.write('{}')
-            conf_file = Path("config.json")
+            with open(CONF_FILE,'x') as cf :
+                cf.write('{}')    
+                
+    @staticmethod
+    def __get_json() -> json:
+        try :
+            with open(CONF_FILE,'r') as cf:
+                content = json.load(cf)
+        except json.JSONDecodeError :
+            content = json.loads('{}')
+        return content
 
-        initiative = json.load(conf_file.open())
+    @staticmethod
+    def get_initiative(windows, ewmh) -> dict():
+        
+        ConfManager.__check_conf_file()
 
+        initiative = ConfManager.__get_json()
+        
         for window in windows:
             if not get_character_name(ewmh.getWmName(window)) in initiative:
                 initiative[get_character_name(ewmh.getWmName(window))] = 0
 
-        with open("config.json",'w') as cf :
+        with open(CONF_FILE,'w') as cf :
             json.dump(initiative, cf)
 
         return initiative
-    
-    
