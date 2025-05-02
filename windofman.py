30e0397e732a857a23ab8b91c50b8e6cc9599a4f
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-05-02 16:28:46 +0200

    format

--- windofman.py
+++ windofman.py
@@ -3,13 +3,14 @@ from keyboardManager import KeyboardManager
 from windowConfig import WindowConfig
 from windowManager import WindowManager
 
+
 def main():
-    
     wm = WindowManager()
     wc = WindowConfig(wm)
     KeyboardManager(wm)
-    
+
     wc.start()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
