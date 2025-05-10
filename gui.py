43830237f7bff06c5cf22310774de6d5c96debe9
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-05-10 20:25:53 +0200

    rework for better scalability

--- gui.py
+++ gui.py
@@ -20,21 +20,22 @@ class GUIApp(tk.Tk):
         self.__init_frame()
 
     def start(self):
-        self.go_page("home")
+        self.go_page(HomePage)
         self.mainloop()
 
     def __init_frame(self):
-        self.frames["home"] = HomePage(self)
-        self.frames["settings"] = SettingsPage(self)
-
-        for frame in self.frames.values():
+        frame_classes = (HomePage, SettingsPage)
+        for F in frame_classes:
+            frame = F(self)
             frame.grid(row=0, column=0, sticky="nsew")
 
-    def go_page(self, page_name):
+            self.frames[F] = frame
+
+    def go_page(self, page):
         try:
-            self.frames[page_name].tkraise()
+            self.frames[page].tkraise()
         except KeyError:
-            print(f"Page {page_name} not found.")
+            print(f"Page {page} not found.")
 
 
 class HomePage(tk.Frame):
@@ -49,7 +50,7 @@ class HomePage(tk.Frame):
         self.refresh_button.pack(pady=10)
 
         self.settings_button = tk.Button(
-            self, text="Settings", command=lambda: self.parent.go_page("settings")
+            self, text="Settings", command=lambda: self.parent.go_page(SettingsPage)
         )
         self.settings_button.pack(pady=10)
 
@@ -68,6 +69,6 @@ class SettingsPage(tk.Frame):
         self.on_top_checkbutton.pack(pady=10)
 
         self.back_button = tk.Button(
-            self, text="Back to Home", command=lambda: self.parent.go_page("home")
+            self, text="Back to Home", command=lambda: self.parent.go_page(HomePage)
         )
         self.back_button.pack(pady=10)
