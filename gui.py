6104154672dba0d919ff9b7d219b64814c9fb5d2
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-05-10 21:21:37 +0200

    load/edit settings

--- gui.py
+++ gui.py
@@ -10,8 +10,17 @@ class GUIApp(tk.Tk):
         self.wm = wm
 
         self.title("Windofman")
-        self.geometry("300x200")
         self.configure(bg=bg_color)
+
+        localization = self.wm.location
+        self.geometry(
+            "300x200" + f"+{localization[0]}+{localization[1]}"
+            if localization[0] and localization[1]
+            else ""
+        )
+        self.resizable(False, False)
+
+        self.load_config()
 
         image = tk.PhotoImage(file="windofman.png")
         self.wm_iconphoto(True, image)
@@ -30,6 +39,10 @@ class GUIApp(tk.Tk):
             frame.grid(row=0, column=0, sticky="nsew")
 
             self.frames[F] = frame
+
+    def load_config(self):
+        self.attributes("-topmost", self.wm.on_top)
+        print(f"Window on top: {self.wm.on_top}")
 
     def go_page(self, page):
         try:
@@ -60,11 +73,18 @@ class SettingsPage(tk.Frame):
         super().__init__(parent)
         self.parent = parent
         self.configure(bg=bg_color)
+
+        # settings
+        self.on_top_var = tk.BooleanVar(value=self.parent.wm.on_top)
+
         self.create_widgets()
 
     def create_widgets(self):
         self.on_top_checkbutton = tk.Checkbutton(
-            self, text="Always on top", bg=bg_color
+            self,
+            text="Always on top",
+            bg=bg_color,
+            variable=self.on_top_var,
         )
         self.on_top_checkbutton.pack(pady=10)
 
@@ -72,3 +92,16 @@ class SettingsPage(tk.Frame):
             self, text="Back to Home", command=lambda: self.parent.go_page(HomePage)
         )
         self.back_button.pack(pady=10)
+
+        self.save_button = tk.Button(
+            self,
+            text="Save Settings",
+            command=self.__save_settings,
+        )
+        self.save_button.pack(pady=10)
+
+    def __save_settings(self):
+        self.parent.wm.on_top = self.on_top_var.get()
+
+        self.parent.wm.save_settings()
+        self.parent.load_config()
