1fd6d1e1ad151d6120ba5902889dd613bb145a67
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-07-26 15:50:28 +0200

    enable only 1 instance

--- windofman.py
+++ windofman.py
@@ -3,10 +3,19 @@ from keyboardManager import KeyboardManager
 from windowManager import WindowManager
 from gui import GUIApp
 
+import fcntl
+import sys
 
 if __name__ == "__main__":
-    wm = WindowManager()
-    km = KeyboardManager(wm)
-    gui = GUIApp(wm, km)
+    lock_file_path = '/tmp/windofman.lock'
+    lock_file = open(lock_file_path, 'w')
+    try:
+        fcntl.flock(lock_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        wm = WindowManager()
+        km = KeyboardManager(wm)
+        gui = GUIApp(wm, km)
 
-    gui.start()
+        gui.start()
+    except BlockingIOError:
+        print("Another instance is already running.")
+        sys.exit(1)
